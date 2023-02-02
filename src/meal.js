1354f0ca1f294ebac85a3b6e5697e200905be0e3
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-02 18:14:01 -0500

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,24 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
 
+function createMenuItem(item) {
+  var newItem = {
+    name: item,
+    price: 10.99,
+    type: "breakfast"
+  }
+  return newItem
+}
+
+function addIngredients(topping, ingredients) {
+  ingredients.push(topping);
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
