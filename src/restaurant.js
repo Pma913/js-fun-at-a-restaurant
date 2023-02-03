b5520d5f0bc888c19264db0937dac2ed225b03d4
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-03 16:21:33 -0500

    Initial commit

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,13 @@
-
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {breakfast: [], lunch: [], dinner: []}
+  };
+  return restaurant;
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem
 }
