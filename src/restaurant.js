752b4a8a4dab12f37f636f2125733e5c8f2ed830
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-03 20:35:11 -0500

    Clear all addMenuItem tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -6,8 +6,27 @@ function createRestaurant(restaurantName) {
   return restaurant;
 }
 
+function addMenuItem(restaurant, menuItem) {
+  // Two parameters [restaurant and menu item]
+  // menu item is an object
+  // add menu object to lunch menu
+  // .push() method?
+  // for (var i = 0; i < restaurant.menus.length; i++) 
+  if (menuItem.type === "breakfast" && !restaurant.menus.breakfast.includes(menuItem)) {
+    restaurant.menus.breakfast.push(menuItem);
+  } else if (menuItem.type === "lunch" && !restaurant.menus.lunch.includes(menuItem)) {
+    restaurant.menus.lunch.push(menuItem);
+  } else if (menuItem.type === "dinner" && !restaurant.menus.dinner.includes(menuItem)) {
+    restaurant.menus.dinner.push(menuItem);
+  } else {
+  }
+  // trigger true or false with comparison operator
+  // operator compare menuItem.name with 
+  // 
+}
+
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
