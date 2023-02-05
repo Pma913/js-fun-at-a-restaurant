bc993b8785d2633d3b96b5b40d2e01a79a75a8ab
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-05 16:59:32 -0500

    Complete remainder of tests in restaurant-test.js file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,3 +1,7 @@
+// declare var set to restaurant
+  // add key for restaurant name set to (parameter)
+  // nest object (menus)
+  // inside menus, nest 3 keys that will each declare an array
 function createRestaurant(restaurantName) {
   var restaurant = {
     name: restaurantName,
@@ -6,27 +10,40 @@ function createRestaurant(restaurantName) {
   return restaurant;
 }
 
-function addMenuItem(restaurant, menuItem) {
-  // Two parameters [restaurant and menu item]
+// Two parameters [restaurant and menu item]
   // menu item is an object
   // add menu object to lunch menu
   // .push() method?
-  // for (var i = 0; i < restaurant.menus.length; i++) 
+  // use comparison operator to be able to check menue type AND if it exists (.includes)
+  // HINT* use if...else
+function addMenuItem(restaurant, menuItem) {
   if (menuItem.type === "breakfast" && !restaurant.menus.breakfast.includes(menuItem)) {
     restaurant.menus.breakfast.push(menuItem);
   } else if (menuItem.type === "lunch" && !restaurant.menus.lunch.includes(menuItem)) {
     restaurant.menus.lunch.push(menuItem);
   } else if (menuItem.type === "dinner" && !restaurant.menus.dinner.includes(menuItem)) {
     restaurant.menus.dinner.push(menuItem);
-  } else {
+  // } else {
   }
-  // trigger true or false with comparison operator
-  // operator compare menuItem.name with 
-  // 
+}
+// remove menue item based on 3 parameters (restaurant, item.name, item.type)
+  // remove the element that contains menuItem(element.name)
+  // run for loop that will search the array that corrosponds with (menuType)
+  // write conditional that checks if the object is in the array, base this off of the itemName
+  // if item exists, then delete. use splice to target specific index value
+  // return statement based off of if item exists or not  
+function removeMenuItem(restaurant, menuItem, menuType) {
+  for (var i = 0; i < restaurant.menus[menuType].length; i++) {
+    if (restaurant.menus[menuType][i].name === menuItem) {
+      restaurant.menus[menuType].splice(i, 1); 
+      return `No one is eating our ${menuItem} - it has been removed from the ${menuType} menu!`
+      } 
+    }
+    return `Sorry, we don't sell ${menuItem}, try adding a new recipe!` 
 }
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
