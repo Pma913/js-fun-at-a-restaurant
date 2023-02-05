5002202474d8b8d27867fadd79df7cba48056383
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-04 21:13:29 -0500

    Add comment and clean up code

--- src/meal.js
+++ src/meal.js
@@ -2,6 +2,10 @@ function nameMenuItem(food) {
   return `Delicious ${food}`;
 }
 
+// take three parameters and make an object
+  // make a variable
+  // set all the parameters to their coresponding key values
+  // return variable
 function createMenuItem(menuItemName, price, type) {
   var newItem = {
     name: menuItemName,
@@ -11,24 +15,33 @@ function createMenuItem(menuItemName, price, type) {
   return newItem
 }
 
+// create function that adds ingredients
+  // take two parameters
+  // test if an argument exists
+  // push unique toppings
 function addIngredients(topping, ingredients) {
   if (ingredients.includes(topping)) {
   } else {
     ingredients.push(topping);
   }
+  return ingredients
 }
-
+// format price
+  // interpolate price
 function formatPrice(price) {
   return `$${price}`;
 } 
 
+// take 10% off price
+  // multiply the original value by 0.1
+  // take the new value and subtract it from original value
 function decreasePrice(value) {
-// multiply the original value by 0.1
-// take the new value and subtract it from original value
-decreasedPrice = value - (value * 0.1);
+var decreasedPrice = value - (value * 0.1);
 return decreasedPrice;
 }
 
+// create recipe from 3 parameters
+  // create variable and that takes in the arguments in coresponding keys
 function createRecipe(title, ingredients, menuItemType) {
   var newRecipe = {
     title: title,
